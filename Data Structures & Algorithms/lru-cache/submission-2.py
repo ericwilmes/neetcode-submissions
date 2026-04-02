50f74a5b5075f04e21b02e56ad170ed85afffe68
Eric Wilmes <89470299+ericwilmes@users.noreply.github.com>
2026-04-02 07:52:08 -0600

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.py
@@ -0,0 +1,19 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        val = self.cache.get(key)
+        if not val:
+            return -1
+        self.cache.move_to_end(key)
+        return val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
