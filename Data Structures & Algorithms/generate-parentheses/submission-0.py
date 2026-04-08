154782b143ad32529338a2ab7b97065b52f49e03
Eric Wilmes <89470299+ericwilmes@users.noreply.github.com>
2026-04-08 07:21:06 -0600

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        output = []
+
+        def ite(curr, opens, closes):
+            if opens > closes:
+                return
+            if opens <= 0 and closes <= 0:
+                output.append(curr)
+
+            # add an open
+            if opens > 0:
+                ite(curr + "(", opens-1, closes)
+
+            # add a close
+            if closes > 0:
+                ite(curr + ")", opens, closes-1)
+                
+
+        ite("(", n-1, n)
+
+        return output
