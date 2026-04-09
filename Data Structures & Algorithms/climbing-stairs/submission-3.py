4c187da218dc1a4053f646ee34f898a761b625b6
Eric Wilmes <89470299+ericwilmes@users.noreply.github.com>
2026-04-09 14:16:30 -0600

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache = {}
+
+        def dfs(curr):
+            if curr > n:
+                return 0
+            if curr in cache:
+                return cache[curr]
+            if curr == n:
+                cache[curr] = 1
+                return 1
+
+            left = dfs(curr + 1)
+            right = dfs(curr + 2)
+
+            res = left + right
+
+            cache[curr] = res
+            return res
+
+        return dfs(0)
